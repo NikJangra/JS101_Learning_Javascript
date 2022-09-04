4d91cc5adb6ff2270903600f3ffe8bf3765e91d3
NikJangra <111484947+NikJangra@users.noreply.github.com>
2022-09-04 22:08:11 +0530

    Update javascript.js
    new edit for pull command

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 let  char = 4585 ;
 
 console.log(typeof(char));
+
+const x = `Name : Nikhil kumar
+school : Masai school
+Grade : Unit 1
+Section : fw22
+Roll number : 0954`;
+
+console.log(x);
+
+var y = `Marks ===>
+HTML = 55
+Css = 56
+JS = 60`
+console.log(y);
+
+
+var z = `Need Improvment
+"(^._.^)"`;
+ console.log(z);
